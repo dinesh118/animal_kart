902b7036bb8b2be0f50b3cc482f6d23eaaa9b426
srikrishnastrive <srikrishna63056@gmail.com>
2025-11-24 06:19:09 -0800

    added the latest changes

--- lib/screens/services/razorpay_service.dart
+++ lib/screens/services/razorpay_service.dart
@@ -3,41 +3,42 @@ import 'package:razorpay_flutter/razorpay_flutter.dart';
 
 class RazorPayService {
   late Razorpay _razorpay;
+
+  final VoidCallback? onPaymentOpen;
   final VoidCallback? onPaymentSuccess;
   final VoidCallback? onPaymentFailed;
-
-  VoidCallback? onPaymentOpen;
-  VoidCallback? onPaymentClose;
+  final VoidCallback? onPaymentClose;
 
   RazorPayService({
     this.onPaymentOpen,
-    this.onPaymentClose,
-    this.onPaymentFailed,
     this.onPaymentSuccess,
+    this.onPaymentFailed,
+    this.onPaymentClose,
   }) {
     _razorpay = Razorpay();
 
     _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handleSuccess);
     _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handleError);
     _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleWallet);
+
+    // Razorpay added a "close" event in newer versions
+    _razorpay.on("RZP_PAYMENT_LINK_CLOSED", _handleClose);
   }
 
   void openPayment({required int amount}) {
-    // Call when process starts
-    if (onPaymentOpen != null) onPaymentOpen!();
+    // Tell UI that payment window is opening
+    onPaymentOpen?.call();
 
     var options = {
       'key': 'rzp_test_ChtIh4impxVRVG',
-      'amount': amount * 100,
+      'amount': amount * 100, // in paise
       'name': 'Markwave Cart',
       'description': 'Buffalo Purchase',
-      'prefill': {'contact': '9876543210', 'email': 'test@gmail.com'},
+      'prefill': {
+        'contact': '9876543210',
+        'email': 'test@gmail.com',
+      },
     };
-
-    // Razorpay opens at this moment
-    Future.delayed(const Duration(milliseconds: 400), () {
-      if (onPaymentClose != null) onPaymentClose!();
-    });
 
     _razorpay.open(options);
   }
@@ -47,14 +48,18 @@ class RazorPayService {
   }
 
   void _handleSuccess(PaymentSuccessResponse response) {
-    if (onPaymentSuccess != null) onPaymentSuccess!();
+    onPaymentSuccess?.call();
   }
 
   void _handleError(PaymentFailureResponse response) {
-    if (onPaymentFailed != null) onPaymentFailed!();
+    onPaymentFailed?.call();
   }
 
   void _handleWallet(ExternalWalletResponse response) {
-    debugPrint("WALLET: $response");
+    debugPrint("WALLET: ${response.walletName}");
+  }
+
+  void _handleClose(dynamic _) {
+    onPaymentClose?.call();
   }
 }
