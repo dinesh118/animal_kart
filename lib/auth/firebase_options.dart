61e62a04a911281dd0f75c81f3451e8ac23e9348
srikrishnam-coder <srikrishnam@markwave.ai>
2025-12-24 21:00:48 +0900

    added the latest changes

--- lib/auth/firebase_options.dart
+++ lib/auth/firebase_options.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
@@ -7,26 +5,52 @@ import 'package:flutter/foundation.dart'
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     if (kIsWeb) {
-      return web;
+      throw UnsupportedError(
+        'DefaultFirebaseOptions have not been configured for web - '
+        'you can reconfigure this by running the FlutterFire CLI again.',
+      );
     }
-    // Since we're targeting Android, we'll use the Android configuration
-    return android;
+    switch (defaultTargetPlatform) {
+      case TargetPlatform.android:
+        return android;
+      case TargetPlatform.iOS:
+        return ios;
+      case TargetPlatform.macOS:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for macos - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.windows:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for windows - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      case TargetPlatform.linux:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions have not been configured for linux - '
+          'you can reconfigure this by running the FlutterFire CLI again.',
+        );
+      default:
+        throw UnsupportedError(
+          'DefaultFirebaseOptions are not supported for this platform.',
+        );
+    }
   }
 
-  static const FirebaseOptions web = FirebaseOptions(
-    apiKey: 'YOUR_API_KEY',
-    appId: 'YOUR_APP_ID',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-    projectId: 'YOUR_PROJECT_ID',
-    authDomain: 'YOUR_AUTH_DOMAIN',
-    storageBucket: 'YOUR_STORAGE_BUCKET',
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'AIzaSyC88x_wjf5oBRmTxyXUwXV_UY2N73kl82c',
+    appId: '1:612299373064:android:5985b830becec8cd0eefbd',
+    messagingSenderId: '612299373064',
+    projectId: 'markwave-481315',
+    storageBucket: 'markwave-481315.firebasestorage.app',
   );
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyAVTVvY8KxJ87tRFS7z3Ppy5P7sU22_pWY',
-    appId: '1:72281267936:android:1a82dda491c756a1dc3a85',
-    messagingSenderId: '72281267936',
-    projectId: 'animalkart-559c3',
-    storageBucket: 'animalkart-559c3.firebasestorage.app',
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'AIzaSyD2v698q2fOZTM8oegi2tq962-wBsLGay8',
+    appId: '1:612299373064:ios:cca415a7d54f55480eefbd',
+    messagingSenderId: '612299373064',
+    projectId: 'markwave-481315',
+    storageBucket: 'markwave-481315.firebasestorage.app',
+    iosBundleId: 'com.markwave.animalcart',
   );
 }
